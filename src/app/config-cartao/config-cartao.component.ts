0687748f84c79c139f35f8cc3151a66f1bd34ff2
Natanael Leite <natansl@gmail.com>
2017-12-29 15:46:54 -0200

    Ajuste no componente de configuração de cartão

--- src/app/config-cartao/config-cartao.component.ts
+++ src/app/config-cartao/config-cartao.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigCartaoComponent implements OnInit {
 
+  public pnlVisivel = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  enviarArte() {
+    this.pnlVisivel = true;
   }
 
 }
